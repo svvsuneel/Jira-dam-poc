e7c35ab7b4b0c60f51b6fd1b75f37636d2399e92
svvsuneel <32540779+svvsuneel@users.noreply.github.com>
2026-04-07 14:41:41 +0530

    Update Dam.py

--- Dam.py
+++ Dam.py
@@ -144,3 +144,8 @@ def webhook():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/test', methods=['GET'])
+def test():
+    print("TEST ENDPOINT HIT")
+    return "OK"
